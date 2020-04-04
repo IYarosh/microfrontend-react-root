0791411e88037cd554aeb859b5610914cc99513b
IYarosh <IYarosh@qulix.com>
2020-04-04 15:55:31 +0300

    Initialize project. Webpack start configuration

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './single-spa-config.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'index.html')})
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+    }
+}
